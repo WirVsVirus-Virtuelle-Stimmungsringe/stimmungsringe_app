a755f35f84938ae225779d76f5a61934b00bd2e0
cc-cpo <codecave@gmx.net>
2020-03-22 12:09:28 +0100

    add endpoints

--- lib/global_constants.dart
+++ lib/global_constants.dart
@@ -1,6 +1,5 @@
-
-
-const _backendBaseUrl = 'http://wvsvhackvirtuellestimmungsringe-env.eba-eug7bzt6.eu-central-1.elasticbeanstalk.com/stimmungsring';
+const _backendBaseUrl =
+    'http://wvsvhackvirtuellestimmungsringe-env.eba-eug7bzt6.eu-central-1.elasticbeanstalk.com/stimmungsring';
 
 String restUrlDashboard() {
   return _backendBaseUrl + '/dashboard';
@@ -10,3 +9,17 @@ String avatarImageUrl(String userId) {
   return _backendBaseUrl + '/images/avatar/' + userId;
 }
 
+String restUrlMyStatus() {
+  return _backendBaseUrl + '/mystatuspage';
+}
+
+String restUrlOtherStatus(String userId) {
+  return _backendBaseUrl + '/otherstatuspage/' + userId;
+}
+
+/**
+ * use to update status
+ */
+String restUrlStatus() {
+  return _backendBaseUrl + '/mystatus';
+}
