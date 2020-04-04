d247526cc7d678dd0b1494f5df6c8fd3fd7d2fe2
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-04-04 07:33:49 +0200

    fix: Update URL to AWS.

--- lib/global_constants.dart
+++ lib/global_constants.dart
@@ -1,6 +1,6 @@
 const _backendBaseUrl =
-//    'http://192.168.178.20:8080/stimmungsring';
-    'http://wvsvhackvirtuellestimmungsringe-env.eba-eug7bzt6.eu-central-1.elasticbeanstalk.com/stimmungsring';
+    'http://192.168.178.40:5000/stimmungsring';
+    //'http://wvsvhackvirtuellestimmungsringe-env.eba-eug7bzt6.eu-central-1.elasticbeanstalk.com/stimmungsring';
 
 const sampleUserMutti = 'cafecafe-b855-46ba-b907-321d2d38beef';
 // note available on EBS yet
