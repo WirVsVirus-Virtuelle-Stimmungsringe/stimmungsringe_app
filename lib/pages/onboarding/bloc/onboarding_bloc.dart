25b7e6be894876d7262b99261c1d4a4974aaace9
Dimitri B <a.geek.dude@protonmail.com>
2020-04-12 21:31:14 +0200

    Beautification

--- lib/pages/onboarding/bloc/onboarding_bloc.dart
+++ lib/pages/onboarding/bloc/onboarding_bloc.dart
@@ -16,54 +16,70 @@ class OnboardingBloc extends Bloc<OnboardingEvent, OnboardingState> {
   @override
   Stream<OnboardingState> mapEventToState(OnboardingEvent event) async* {
     if (event is CheckUser) {
-      final SigninUserResponse signinUserResponse =
-          await onboardingRepository.signin(currentDeviceIdentifier);
-      currentUserId = signinUserResponse.userId;
-
-      if (signinUserResponse.hasGroup) {
-        currentGroupId = signinUserResponse.groupId;
-        currentGroupName = signinUserResponse.groupName;
-
-        yield GotoDashboard();
-      } else {
-        yield OnboardingIntro();
-      }
+      yield* _mapCheckUserToState(event);
+    } else if (event is BeginStartNewGroup) {
+      yield* _mapBeginStartNewGroupToState(event);
+    } else if (event is StartNewGroup) {
+      yield* _mapStartNewGroupToState(event);
+    } else if (event is BeginJoinGroup) {
+      yield* _mapBeginJoinGroupToState(event);
+    } else if (event is SearchGroup) {
+      yield* _mapSearchGroupToState(event);
     }
+  }
 
-    if (event is BeginStartNewGroup) {
+  Stream<OnboardingState> _mapCheckUserToState(CheckUser checkUser) async* {
+    final SigninUserResponse signinUserResponse =
+        await onboardingRepository.signin(currentDeviceIdentifier);
+    currentUserId = signinUserResponse.userId;
+
+    if (signinUserResponse.hasGroup) {
+      currentGroupId = signinUserResponse.groupId;
+      currentGroupName = signinUserResponse.groupName;
+
+      yield GotoDashboard();
+    } else {
+      yield OnboardingIntro();
+    }
+  }
+
+  Stream<OnboardingState> _mapBeginStartNewGroupToState(
+      BeginStartNewGroup beginStartNewGroup) async* {
+    yield StartNewGroupInitial();
+  }
+
+  Stream<OnboardingState> _mapStartNewGroupToState(
+      StartNewGroup startNewGroup) async* {
+    final StartNewGroupResponse startNewGroupResponse =
+        await onboardingRepository.startNewGroup(startNewGroup.groupName);
+    if (startNewGroupResponse != null) {
+      currentGroupId = startNewGroupResponse.groupId;
+      currentGroupName = startNewGroupResponse.groupName;
+      yield StartNewGroupSuccess(groupName: startNewGroup.groupName);
+    } else {
+      yield StartNewGroupFailedConflict(groupName: startNewGroup.groupName);
       yield StartNewGroupInitial();
     }
+  }
 
-    if (event is StartNewGroup) {
-      final StartNewGroupResponse startNewGroupResponse =
-          await onboardingRepository.startNewGroup(event.groupName);
-      if (startNewGroupResponse != null) {
-        currentGroupId = startNewGroupResponse.groupId;
-        currentGroupName = startNewGroupResponse.groupName;
-        yield StartNewGroupSuccess(groupName: event.groupName);
-      } else {
-        yield StartNewGroupFailedConflict(groupName: event.groupName);
-        yield StartNewGroupInitial();
-      }
-    }
+  Stream<OnboardingState> _mapBeginJoinGroupToState(
+      BeginJoinGroup beginJoinGroup) async* {
+    yield FindGroupInitial();
+  }
 
-    if (event is BeginJoinGroup) {
+  Stream<OnboardingState> _mapSearchGroupToState(
+      SearchGroup searchGroup) async* {
+    final FindGroupResponse findGroupResponse = await onboardingRepository
+        .findGroupByName(searchGroup.groupCode); // note groupCode==groupName
+
+    if (findGroupResponse != null) {
+      await onboardingRepository.joinGroup(findGroupResponse.groupId);
+      currentGroupId = findGroupResponse.groupId;
+      currentGroupName = findGroupResponse.groupName;
+      yield FindGroupSuccess(groupName: findGroupResponse.groupName);
+    } else {
+      yield FindGroupNotFound();
       yield FindGroupInitial();
-    }
-
-    if (event is SearchGroup) {
-      final FindGroupResponse findGroupResponse = await onboardingRepository
-          .findGroupByName(event.groupCode); // note groupCode==groupName
-
-      if (findGroupResponse != null) {
-        await onboardingRepository.joinGroup(findGroupResponse.groupId);
-        currentGroupId = findGroupResponse.groupId;
-        currentGroupName = findGroupResponse.groupName;
-        yield FindGroupSuccess(groupName: findGroupResponse.groupName);
-      } else {
-        yield FindGroupNotFound();
-        yield FindGroupInitial();
-      }
     }
   }
 
